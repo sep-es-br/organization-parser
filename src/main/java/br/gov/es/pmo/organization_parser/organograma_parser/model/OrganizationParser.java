99b0e4b341f560204d87371842568a80d7e41f93
Bruno Matos <brunomatosbarbosa@hotmail.com>
2026-05-25 13:39:39 -0300

    ajustes na classe organizationparser

--- src/main/java/br/gov/es/pmo/organization_parser/organograma_parser/model/OrganizationParser.java
+++ src/main/java/br/gov/es/pmo/organization_parser/organograma_parser/model/OrganizationParser.java
@@ -1,56 +1,76 @@
 package br.gov.es.pmo.organization_parser.organograma_parser.model;
 
-import org.json.JSONObject;
+import br.gov.es.pmo.organization_parser.pmo_base.model.IOrganizationParser;
+import br.gov.es.pmo.organization_parser.pmo_base.model.OrganizationDto;
+import br.gov.es.pmo.organization_parser.pmo_base.utils.ApiClient;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
-import br.gov.es.pmo.organization_parser.pmo_base.model.IOrganizationParser;
-import br.gov.es.pmo.organization_parser.pmo_base.utils.ApiClient;
-
 @Component("org")
-public class OrganizationParser implements IOrganizationParser<String> {
+public class OrganizationParser
+    implements IOrganizationParser<String> {
 
+    private static final String INTEGRATION = "GOVES";
+    private static final String SUFFIX = "ES";
     private static final String GUID = "guid";
     private static final String SIGLA = "sigla";
     private static final String RAZAO_SOCIAL = "razaoSocial";
 
-    private static final String GOVES_GUID = "fe88eb2a-a1f3-4cb1-a684-87317baf5a57";
+    private static final String GOVES_GUID =
+        "fe88eb2a-a1f3-4cb1-a684-87317baf5a57";
 
     private final ApiClient apiClient =
         new ApiClient("https://api.organograma.es.gov.br");
 
-    private JSONObject getOrganization(String clientToken) {
+    public List<OrganizationDto> getOrganizations(String token) {
 
-        JSONObject resp = apiClient
-        .doGetRequest(
-            "/organizacoes/" + GOVES_GUID + "/filhas",
-            clientToken
-        )
-        .block();
+        String json =
+            apiClient
+                .doGetRequest(
+                    "/organizacoes/"
+                        + GOVES_GUID
+                        + "/filhas",
+                    token
+                )
+                .block();
 
-        return resp;
+        try {
+              JSONArray array =
+                (JSONArray) new JSONParser(
+                    JSONParser.DEFAULT_PERMISSIVE_MODE
+                ).parse(json);
+
+            List<OrganizationDto> result = new ArrayList<>();
+
+            for (Object obj : array) {
+
+                JSONObject o = (JSONObject) obj;
+
+                OrganizationDto dto = new OrganizationDto(
+                    (String) o.get(GUID),
+                    (String) o.get(SIGLA),
+                    (String) o.get(RAZAO_SOCIAL),
+                    INTEGRATION,
+                    SUFFIX
+                );
+
+                result.add(dto);
+            }
+
+            return result;
+
+        } catch (Exception e) {
+            throw new RuntimeException(
+                "Erro ao parsear organizações",
+                e
+            );
+        }
     }
 
-    @Override
-    public String getId(String clientToken) {
-
-        JSONObject organization = getOrganization(clientToken);
-
-        return organization.optString(GUID, null);
-    }
-
-    @Override
-    public String getNome(String clientToken) {
-
-        JSONObject organization = getOrganization(clientToken);
-
-        return organization.optString(SIGLA, null);
-    }
-
-    @Override
-    public String getFullName(String clientToken) {
-
-        JSONObject organization = getOrganization(clientToken);
-
-        return organization.optString(RAZAO_SOCIAL, null);
-    }
 }
